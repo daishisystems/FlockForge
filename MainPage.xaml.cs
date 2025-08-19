8e26889073da10b2d7edf9b52e35d4d330f55ed0
Paul Mooney <daishisystems@users.noreply.github.com>
2025-08-19 14:48:45 +0200

    Remove reports tab and fix profile navigation

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -15,16 +15,13 @@ public partial class MainPage : ContentPage, IDisposable
     }
 
     private async void OnProfileTapped(object? sender, EventArgs e) =>
-        await Shell.Current.GoToAsync("profile");
+        await Shell.Current.GoToAsync("///profile");
 
     private async void OnFarmManagementTapped(object? sender, EventArgs e) =>
-        await Shell.Current.GoToAsync("farms");
+        await Shell.Current.GoToAsync("///farms");
 
     private async void OnGroupsTapped(object? sender, EventArgs e) =>
-        await Shell.Current.GoToAsync("groups");
-
-    private async void OnReportsTapped(object? sender, EventArgs e) =>
-        await Shell.Current.GoToAsync("reports");
+        await Shell.Current.GoToAsync("///groups");
 
     protected override void OnDisappearing()
     {
