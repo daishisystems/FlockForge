22824103f9f8344303902f8fa9d3fe6d70190e24
Paul Mooney <daishisystems@users.noreply.github.com>
2025-08-19 14:33:57 +0200

    Refactor navigation with flyout and dashboard

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -124,7 +124,7 @@ public partial class AppShell : Shell
             if (MainTabBar != null) MainTabBar.IsVisible = true;
             else _logger.LogWarning("MainTabBar is null in ShowMainApplication");
 
-            FlyoutBehavior = FlyoutBehavior.Disabled;
+            FlyoutBehavior = FlyoutBehavior.Flyout;
 
             MainThread.BeginInvokeOnMainThread(async () =>
             {
